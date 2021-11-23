cd0d25836630eff8bf1da2373077bf0f4ebe49b9
YuliiaPanchuk <jula1panchuk@gmail.com>
2021-11-23 22:09:33 +0100

    Init commit

---
+++ Task3/Task3.cpp
@@ -0,0 +1,17 @@
+// Preprocessor directives
+#include <iomanip>
+#include <iostream>
+using namespace std;int main(){	// Define and initialize variables
+	int start = 1;
+	int end = 15;
+	int calcOdds;
+
+	for (int start = 0; start < end; start++)
+	{
+
+	}
+	
+	// Read input
+	system("CLS");
+	cout << "The product of odd integers" << endl;
+	cout << "============" << endl << endl;	// Calculation	// Display result	cout << "The product of all odd integers between " 		<< start << " and " << end << " is " 		<< calcOdds << endl;}
